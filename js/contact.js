9e7b99f9071b5760d533b06959115272371bbca8
Namaz Gurbanov <namaz.gurbanov@yahoo.com>
2020-08-23 10:26:41 -0500

    send ip address in message

--- js/contact.js
+++ js/contact.js
@@ -2,13 +2,19 @@ $(document).ready(function() {
     $('#contactForm').submit(function(e) {
         e.preventDefault();
         $.ajax({
-            url: "http://faridagurbanova.com:4000/api/contact",
-            type: "post",
-            data: $('#contactForm').serialize(),
-            success: function() {
-                document.getElementById('name').value = '';
-                document.getElementById('email').value = '';
-                document.getElementById('message').value = '';
+            url: "http://api.ipify.org/?format=json",
+            type: "get",
+            success: function(visitor) {
+                $.ajax({
+                    url: "http://faridagurbanova.com:4000/api/contact",
+                    type: "post",
+                    data: $('#contactForm').serialize() + '&ip=' + visitor.ip,
+                    success: function() {
+                        document.getElementById('name').value = '';
+                        document.getElementById('email').value = '';
+                        document.getElementById('message').value = '';
+                    }
+                });
             }
         });
     });
